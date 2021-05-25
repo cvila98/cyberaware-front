c668b8e0512dfe1b11262d089c0e8acfffa9fbf2
cvila98 <cvila98@gmail.com>
2021-05-25 19:48:11 +0200

    create page question widget

---
+++ lib/screens/formacions/question.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class QuestionWidget extends StatefulWidget{
+
+  @override
+  _QuestionWidgetState createState() => _QuestionWidgetState();
+}
+
+class _QuestionWidgetState extends State<QuestionWidget>{
+
+  @override
+  Widget build(BuildContext context) {
+  }
+
+}
